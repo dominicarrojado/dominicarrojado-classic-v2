3bb61e0cd7413b79b3646470c581768e0d317959
dominic_arrojado <dominic.arrojado@razer.com>
2019-10-20 00:48:01 +0800

    Updates

---
+++ precache-manifest.a8e688cca21ce096ff9a11ccd2c67374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fb0a95c383e6723ea296989ed3255b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1167166c4764c36587",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6447f467fa1e40a5a7de",
+    "url": "/static/js/2.5d1ba154.chunk.js"
+  },
+  {
+    "revision": "4a1167166c4764c36587",
+    "url": "/static/js/main.a7fabaf7.chunk.js"
+  },
+  {
+    "revision": "7f02d02d7456f456b0e3",
+    "url": "/static/js/runtime-main.bc2394fd.js"
+  }
+]);
